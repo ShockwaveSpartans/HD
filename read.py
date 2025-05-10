55ade6e2750876159dbccb16d59d245a397f8894
MuahahahYY <ylim0097@student.monash.edu>
2025-05-10 22:27:29 +0800

    Version_3

--- read.py
+++ read.py
@@ -1,51 +1,44 @@
 import serial
-import time
-import numpy as np
-import wave
+import sys, subprocess, time, os
 
-sampleRate = 6400  # bytes per second
-recordTime = 15      # seconds
+# Configuration
+CHUNK_SIZE = 500         # Size of each read
+SAMPLE_RATE = 6400
+TIME = 10
+#the wav file will be 5 second
+TOTAL_BYTES = TIME * SAMPLE_RATE 
+#TOTAL_BYTES = 100 * 1024  
 
-ser = serial.Serial(port="/dev/tty.usbmodem103", baudrate=230400, bytesize=8, parity="N", stopbits=1, timeout=5)
-print(ser.name)
+# Open serial port
+ser = serial.Serial(port="COM10", baudrate = 230400, bytesize=8, parity="N", stopbits=1, timeout=5)
 
-data = []
-data2 = []
+print(f"[INFO] Serial port opened: {ser.name}")
 
-num_bytes_to_read = sampleRate * recordTime
+# Open binary file for writing raw ADC data
+with open("Transfer_ADC_values.data", "ab") as file:
+    bytes_read = 0
+    while bytes_read < TOTAL_BYTES:
+        chunk = ser.read(CHUNK_SIZE)  # Read 500 bytes
+        if not chunk:  # Handle potential read errors
+            print("\n No data received. Check serial connection.")
+            break
+        file.write(chunk)             # Write to file
+        bytes_read += len(chunk)
+        print(f"[INFO] {bytes_read}/{TOTAL_BYTES} bytes received...", end='\r')
 
-f = open("raw_data.txt", "w")
-
-for i in range(num_bytes_to_read):
-    byte = ser.read(2) # transmit two bytes, 12 bits
-    if byte:
-        result = int(str(byte[0]) + str(byte[1]))
-        data.append(result) # result will not be larger than 4095 or lesser than 0
-        f.write(f"{result}\n")
-
-f.close()
-
-data = np.array(data)
-
-
-data = data / 4095  # scale 12-bit range to [0, 1]
-print(data)
-
-data = (data * 2 - 1) * 32767  # Scale to [-32767, 32767]    
-# Scale to [-32767, 32767]
-
-data = data.astype(np.uint16)
-print(data)
-
-wav_file = wave.open("raw_ADC_values.wav","wb")
-wav_file.setnchannels(1)
-wav_file.setsampwidth(2)
-wav_file.setframerate(sampleRate)
-wav_file.writeframes(data.tobytes())
-wav_file.close()
-
-
-print(f"Recorded {len(data)} bytes in {recordTime} seconds.")
-
+# Close serial port
 ser.close()
-print(f"Created raw_data.txt with {num_bytes_to_read} samples.")
+print("\nData sampling complete.")
+
+
+
+print("\n")
+file.close()
+#file_2.close()
+ser.close()
+compileResult = subprocess.getstatusoutput(f"gcc read_serial.c -o read_serial")
+codeOutput = subprocess.getstatusoutput(f"read_serial Transfer_ADC_values.data transfer.wav") #runs the executable with command line arguments
+print(compileResult[0]) #prints 0 if compiled with no issues
+print(compileResult[1]) #prints compilation errors, if any
+print(codeOutput[0]) #prints 0 if the C program could run without any errors
+print(codeOutput[1]) #prints any output displayed onto the terminal by the C program
